37cc241b53361a97015f41cc0f2bf43e6d817e10
Luis <lf.hdez@hotmail.com>
2024-06-15 15:09:28 -0600

    hola

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,7 @@
 <script>
-	import { Button } from '@components/ui/button'
+	
 </script>
 
 <main>
 	<h1 class="text-5xl text-blue-600">App Cliente con Svelte y TailwindCSS</h1>
-	<Button>Click me</Button>
 </main>
